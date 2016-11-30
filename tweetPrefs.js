a93beeb237744ee0c3e07c66ff5825151b8159cb
Alexis Reyes <arreyes20285@gmail.com>
2016-11-29 21:50:29 -0500

    Creating the settings file for easy changes

--- tweetPrefs.js
+++ tweetPrefs.js
@@ -4,8 +4,14 @@
  * result_type
  * lang
  */
-module.exports = {
-    q: '#sloth OR #slothlove',
-    result_type: 'recent',
-    lang: 'en'
+module.exports = 
+    params: {
+        q: '#sloth OR #slothlove', // https://dev.twitter.com/rest/public/search
+        result_type: 'recent',
+        lang: 'en'
+    },
+    frequency: {
+        fav: 4.5 * (3600 * 1000), //Hours in Miliseconds
+        retweet: 1 * (3600 * 1000)
+    }
 }
